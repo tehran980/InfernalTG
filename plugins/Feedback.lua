36b345923379f7301c869e17170ecf30bb3f1511
tehran980 <hsn.m@torkmail.ir>
2016-01-16 05:16:32 -0800

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -21,7 +21,7 @@ local fuses = '!printf user#id' .. msg.from.id
 
                  local sends0 = send_msg('chat#59845789', fuse, ok_cb, false)
 
- return 'Your request has been sended to @Arashinfernal 😜!'
+ return 'Your request has been sended 😜!'
 
      
 
