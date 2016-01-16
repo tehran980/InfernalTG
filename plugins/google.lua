1885328b447750495fa132ab7eb2af9b2750bc64
tehran980 <hsn.m@torkmail.ir>
2016-01-16 08:25:42 -0800

    Update google.lua

--- plugins/google.lua
+++ plugins/google.lua
@@ -39,9 +39,9 @@ do
 
   return {
     description = 'Returns five results from Google. Safe search is enabled by default.',
-    usage = ' !google [terms]: Searches Google and send results',
+    usage = ' google [terms]: Searches Google and send results',
     patterns = {
-      '^!google (.*)$',
+      '^[Gg]oogle (.*)$',
       '^%.[g|G]oogle (.*)$'
     },
     run = run
