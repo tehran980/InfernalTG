60bb306c999f1e25b4c9d412512f8dd5e313f257
tehran980 <hsn.m@torkmail.ir>
2016-01-16 04:19:47 -0800

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,17 @@
+ do
+
+function run(msg, matches)
+  return "1000 toman 6 mahe"  matches[1]
+end
+
+return {
+  description = "Says nekh to someone", 
+  usage = "get nerkhgroup to by sending نرخ ",
+  patterns = {
+    "^نرخ$",
+    "^نرخ$",
+  }, 
+  run = run 
+}
+
+end
